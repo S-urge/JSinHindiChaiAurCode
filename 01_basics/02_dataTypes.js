28a0addbc604220f31f2ea5cee2e8e1c704fe8a9
Ramesh Choudhary <122167984+S-urge@users.noreply.github.com>
2026-01-13 11:02:04 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) // we are using NodeJs, not browser
+
+console.log(3+3)
+
+console.log("Hello")
+
+let name = "Ramesh"
+let age = 18
+let isLoggedIn = false
+
+// Range of Number is about 2^53. If greater than this 
+// than we might need bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => Variable declared but not assigned
+// symbol => Unique
+
+
+// Object
+
+console.log(typeof("Ramesh"))
+console.log(typeof(null))
+console.log(typeof(undefined))
+
+// VISIT ECMA AND READ SPECIFICATIONS
